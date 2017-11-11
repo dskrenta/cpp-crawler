5d9d29b18a8ea277ae65024629ca5f62fca173a5
dskrenta <dskrenta@gmail.com>
2017-11-11 15:08:46 -0700

    basic file setup

---
+++ src/Crawler.cpp
@@ -0,0 +1,14 @@
+// Author: David Skrenta CS1300 Fall 2017
+// Recitation: 210 - Arcadia
+// Assignment 8
+// Crawler.cpp
+
+#include "Crawler.h"
+
+Crawler::Crawler() {
+
+}
+
+Crawler::~Crawler() {
+  
+}
---
+++ src/Crawler.h
@@ -0,0 +1,20 @@
+// Author: David Skrenta CS1300 Fall 2017
+// Recitation: 210 - Arcadia
+// Assignment 8
+// Crawler.h
+
+#ifndef CRAWLER_H
+#define CRAWLER_H
+
+#include <iostream>
+
+using namespace std;
+
+class Crawler {
+    public:
+        Crawler();
+        ~Crawler();
+
+    private:
+};
+#endif // CRAWLER_H
